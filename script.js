64f3ee555981f95b0d81bc0aef4eedb52390c017
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2024-10-07 12:03:17 +0100

    Add the function that will creates the grid

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const boxesContainer = document.querySelector('.boxesContainer');
+const wideBoxesHolder = [];
+
+function createGrid(gridSize)
+{
+    for(let i = 0; i < gridSize; i++){
+        const wideBox = document.createElement('div');
+        wideBox.classList.add('wide-box');
+        for (let j = 0; j < gridSize; j++){
+            const tinyBox = document.createElement('div');
+            tinyBox.classList.add('tiny-box');
+            wideBox.appendChild(tinyBox);
+        }
+        wideBoxesHolder.push(wideBox);
+
+    }
+
+    for(let i = 0; i < gridSize; i++){
+        boxesContainer.appendChild(wideBoxesHolder[i]);
+    }
+}
