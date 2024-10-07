a5f0b2a81ea4e952979de98348e74386aed76fe3
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2024-10-07 16:11:40 +0100

    Add a gridResizing btn

--- script.js
+++ script.js
@@ -3,6 +3,48 @@
 const boxesContainer = document.querySelector('#boxesContainer');
 const wideBoxesHolder = [];
 
+const resizingBtn = document.querySelector('.resize');
+resizingBtn.addEventListener('click',resizeGrid);
+
+let gridSize = 0;
+
+function resizeGrid(){
+    // remove the current wideBoxes from DOM
+    for(let i = 0; i < wideBoxesHolder.length; i++){
+        boxesContainer.removeChild(wideBoxesHolder[i]);
+    }
+
+    // Free Up the wideBoxesHolder array
+    while(wideBoxesHolder.length > 0){
+        wideBoxesHolder.pop();
+    }
+
+    gridSize = getNewGridSize();
+
+    createGrid(gridSize);
+    
+}
+
+function getNewGridSize(){
+    let newGridSize = 0;
+    newGridSize = +prompt("Enter your new Grid size:\nyour number should be an Integer that is greater than 0, and less or equal to 100")
+    while((newGridSize <= 0) || (newGridSize > 100) || (Number.isInteger(newGridSize) === false))
+        {
+            newGridSize = +prompt('Invalid number, your number should be an Integer that is greater than 0 and less or equal to 100');
+        }
+    return newGridSize;
+}
+
+function getGridSizeAndDrawIt(){
+    let gridSize = +prompt("Change the grid size:\n Your number should fall in ]0, 100] interval");
+    while((gridSize <= 0) || (gridSize > 100) || (Number.isInteger(gridSize) === false))
+    {
+        gridSize = +prompt('Invalid number, your number should be an Integer that is greater than 0 and less or equal to 100');
+    }
+    alert(`Your grid size: ${gridSize}`);
+    createGrid(gridSize);
+}
+
 function createGrid(gridSize)
 {
     for(let i = 0; i < gridSize; i++){
